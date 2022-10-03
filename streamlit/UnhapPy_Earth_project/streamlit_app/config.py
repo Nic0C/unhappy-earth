93d34c2167df38ce31abd2df2bb9b71a9eb61d04
Boris Baldassari <boris.baldassari@gmail.com>
2022-10-03 08:50:40 +0200

    Fix links to linkedin and github profiles.

--- streamlit/UnhapPy_Earth_project/streamlit_app/config.py
+++ streamlit/UnhapPy_Earth_project/streamlit_app/config.py
@@ -11,13 +11,13 @@ TITLE = "UnhapPy Earth"
 
 TEAM_MEMBERS = [
     Member(name = "Olga Fedorova", 
-           linkedin_url = "https://www.linkedin.com/in/charlessuttonprofile/", 
-           github_url = "https://github.com/charlessutton"),
+           linkedin_url = "https://www.linkedin.com/in/olga-fedorova-665a4b63/", 
+           github_url = "https://github.com/OlgaFedorovaKukk"),
     Member(name = "Boris Baldassari", 
-           linkedin_url = "https://www.linkedin.com/in/charlessuttonprofile/", 
-           github_url = "https://github.com/charlessutton"),
+           linkedin_url = "https://www.linkedin.com/in/borisbaldassari/", 
+           github_url = "https://github.com/borisbaldassari"),
     Member(name = "Nicolas Cristiano", 
-           linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/", 
-           github_url="https://github.com/charlessutton")]
+           linkedin_url="https://www.linkedin.com/in/nicolas-cristiano-7b8a23171/", 
+           github_url="https://github.com/Nic0C")]
 
 PROMOTION = "Promotion Continue Data Analyst - Mars 2022"
