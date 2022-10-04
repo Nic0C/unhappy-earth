50171153fcf0740f94588194b169773ccb002615
OlgaFedorovaKukk <106063711+OlgaFedorovaKukk@users.noreply.github.com>
2022-10-05 00:05:28 +0200

    Update partie prédiction.  Tout come sur Report.

--- streamlit/tabs/prediction.py
+++ streamlit/tabs/prediction.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import datetime as datetime
 from prophet import Prophet
 
 
@@ -17,67 +16,46 @@ def run():
     
     st.title("Modélisation / prédiction de la température sur les prochaines années.")
     
-    st.markdown("Parmi les différents modèles, nous avons choisi Facebook Prophet. Prophet permet la prédiction des données de séries temporelles, en se basant sur un modèle additif : aux tendances non-linéaires s'ajoute une composante liée à la saisonnalité. Cette librairie est donc particulièremet adaptée à notre étude.")
+    st.subheader("Pouvons-nous prédire les températures sur les prochaines décennies ?")
     
-    st.markdown("Nous allons filtrer quelques colonnes de notre data set 'global_land'. NeuralProphet ne s’attend qu’à deux colonnes (temporalité / donnée). De plus, concentrons nous sur la période où la tendance croissante de la température est constante et forte, c'est à dire à partir de 1975:")
+    st.markdown("Parmi différents modèles, nous avons choisi Facebook Prophet. Prophet permet la prédiction de données de séries temporelles. Nous avons testé le modèle multiplicatif et le modèle additif de Facebook Prophet et nous avons vu que le deuxième correspond mieux à nos données: aux tendances non-linéaires s'ajoute une composante liée à la saisonnalité. Cette librairie est donc particulièrement adaptée à notre étude.")
+    
+    st.markdown("Nous allons nous concentrer sur la période où la tendance croissante de la température est constante et forte, c'est-à-dire à partir de 1975.")
 
+    st.markdown("Dans le graphique suivant nous visualisons bien que pendant la période comprise entre 1975 et 2022 la tendance de la température absolue est explicite, croissante, constante et forte (augmentation de la tendance de 8,7 °C en 1975 jusqu’à 10 °C en 2022) :")
+
+
+#Definition des donnes a traiter: 
     new_column = df.loc[(df['date'] >= '1975-01-15')] 
     new_column = new_column[['date', 'abs']] 
     new_column.dropna(inplace=True)
     new_column.columns = ['ds', 'y'] 
-    new_column.tail()  
 
-#Premier tableau:
-    st.write(new_column.tail()) 
-
-    st.markdown("Nous visualisons bien que pendant la période entre 1975 et 2022 la tendance de la température absolue est explicite, croissante, constante et forte :")    
-
-#Premier graphique:
-    import altair as alt
-
-    chart = (
-        alt.Chart(
-            data = new_column,
-            title = "Température absolue entre 1975 et 2022 (données mensuelles)",
-        )
-        .mark_line()
-        .encode(
-            x = alt.X("ds", axis = alt.Axis(labelOverlap="greedy",grid=False, title = "Date")),
-            y = alt.Y("y", axis = alt.Axis(title = "Température absolue en °C"))
-            ).properties(height = 360, width = 140)
-        )
-
-    st.altair_chart(chart, use_container_width = True)
-
-    st.markdown("Prédiction de la température sur les 50 prochaines années :")
-
-
-    # Affichage graphe evolution températures / tendance:
+# Affichage graphe evolution températures / tendance:
     fig, ax1 = plt.subplots(figsize=(16,4))
-
-
-    ax1.plot(new_column['ds'], new_column['y'], label="Temperature absolue")
+    formatter = mdates.DateFormatter("%Y") ### formatter of the date
     
-    ax2 = ax1.twinx()
+    ax1.xaxis.set_major_formatter(formatter) ## calling the formatter for the x-axis   
+    
+    ax1.plot(mdates.date2num(new_column['ds']), new_column['y'], label="Temperature absolue")    
 
     x = mdates.date2num(new_column['ds'])
-    y= new_column['y']
-    z = np.polyfit(x, y, 1)
+    z = np.polyfit(x, new_column['y'], 1)
     p = np.poly1d(z)
       
-    ax2.plot(x,p(x),"r--")
-    ax1.set_xlabel("Date")
+    ax1.plot(x,p(x),"r--", label="Tendance")
+    ax1.set_xlabel("Date (par annnée)")
     ax1.set_ylabel("Température absolue en °C")
     plt.title("Température absolue entre 1975 et 2022 (données mensuelles)")
-
+    plt.grid(color='grey', alpha=0.2)
 
     fig.legend(loc='upper center', bbox_to_anchor=(0.28, 0.85))
     
     st.pyplot(fig)
 
 
-#Prediction Prohet    
-    st.markdown("Prédiction de la température sur les 50 prochaines années :")
+#Prediction Prophet    
+    st.markdown("A partir de nos données, Facebook Prophet calcule la prédiction suivante pour les températures sur les 50 prochaines années:")
 
     m = Prophet(seasonality_mode='additive').fit(new_column)
     future = m.make_future_dataframe(periods=600, freq='M')
@@ -88,3 +66,25 @@ def run():
     import warnings
     warnings.filterwarnings("ignore")
     st.pyplot(fig)
+
+    st.markdown("La prédiction qui commence à partir de l'année 2022 sur ce graphique, nous montre toujours une évolution de la température à la hausse. ")
+
+#Presentation des components: tendance et la déviation
+    st.markdown("Pour plus de détail, visualisons 2 composantes de la prédiction :")
+    st.markdown("- la tendance ;")
+    st.markdown("- les déviations saisonnières de cette tendance :")
+    
+    fig = m.plot_components(fcst, figsize=(16,8))
+    fig.suptitle("Prediction de la tendance de la température absolue et de la variation saisonnière", y=1.02, fontsize=14)
+    axes = fig.get_axes()
+
+    axes[0].set_xlabel('Date (par annnée)')
+    axes[0].set_ylabel('Température absolue en °C')
+    axes[1].set_xlabel("Mois de l'année")
+    axes[1].set_ylabel('Var saisonnière de la temp par rapport à la tendance, en°C')
+    warnings.filterwarnings("ignore")
+    st.pyplot(fig)
+    
+#Conclusion
+    st.markdown("Notre prévision repose sur le principe que la température globale absolue va suivre la même tendance que ces 46 dernières années, sans action climatique majeure. Dans ce cas, notre modèle prédit une croissance approximative de 1.9 °C dans 50 ans (10 °C en 2021, jusqu’à 11,9 °C en 2072) . En comparaison avec la multitude d'études prévisionnelles qui existent actuellement, notre résultat semble juste et raisonnable, mais bien peu optimiste.")
+    
